d2c152d232fd730c86343345b27829d381e4f889
darrenthebozz <darrenthebozz1@gmail.com>
2023-03-31 11:17:07 +0000

    Tweaked command line argument spacing

--- bin2c.c
+++ bin2c.c
@@ -38,7 +38,7 @@ main(int argc, char *argv[])
     if (argc < 4) {
         fprintf(stderr, "Usage: %s binary_file output_file array_name\n"
                         "Command line arguments: \n"
-                        "-a|--append                  appends to output file instead of overwriting\n",
+                        "  -a|--append                  appends to output file instead of overwriting\n",
                 argv[0]);
         return -1;
     }
